de095848dd05acb283b0e5f9ea3876d5933bf4ec
Daniele Pizzi <pizzi.daniele1993@gmail.com>
2022-11-15 22:06:05 +0100

    add comment

--- LoRaReceiver/LoRaReceiver.ino
+++ LoRaReceiver/LoRaReceiver.ino
@@ -107,7 +107,7 @@ void onReceive(int packetSize)
       http.addHeader("Content-Type", "application/json");
       
       // Data to send with HTTP POST
-      String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&value1=24.25&value2=49.54&value3=1005.14";
+      //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&value1=24.25&value2=49.54&value3=1005.14";
       
       // Send HTTP POST request
       int httpResponseCode = http.POST(loRaData);  
